ada9762948d13967a70b89906f6056c4e34190ea
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2020-09-14 01:53:31 +0530

    Create digitAddition.java

---
+++ java/digitAddition.java
@@ -0,0 +1,13 @@
+class mAdditionAndAverage{
+	public static void main(String[] args) {
+		double num1, num2, sum, avg;
+		num1 = 45;
+		num2 = 88;
+		System.out.println("First number: " + num1);
+		System.out.println("Second number: " + num2);
+		sum = num1 + num2;
+		System.out.println("Sum: " + sum);
+		avg = sum/2.0;
+		System.out.println("Average: " + avg);
+	}
+}
