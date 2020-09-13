42a4edea58f1d25d59fd10fb9f00141f4966fb43
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2020-09-14 01:59:04 +0530

    Create starPattern1.java

---
+++ java/starPattern1.java
@@ -0,0 +1,13 @@
+class pattern1{
+	public static void main(String args[]){
+		int x=5,i,j;
+		for(i=1;i<=x;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
