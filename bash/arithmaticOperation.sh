8b6bf25ab0516e6b5c21aec37cc390c73df3f61d
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2020-09-14 01:49:12 +0530

    Create arithmaticOperation.sh

---
+++ bash/arithmaticOperation.sh
@@ -0,0 +1,12 @@
+echo "Enter one number:"
+read n
+echo "Enter another number:"
+read m
+sum=$(( n + m))
+echo "Sum: $sum"
+sub=$(( n - m ))
+echo "Sub: $sub"
+mul=$(( n * m ))
+echo "Mul: $mul"
+div=$(( n / m))
+echo "Div: $div"
