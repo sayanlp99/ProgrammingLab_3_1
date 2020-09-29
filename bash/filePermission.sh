974376ad77be098fa1c336cae7a0f9632745806e
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2020-09-29 03:55:31 -0700

    Update filePermission.sh

--- bash/filePermission.sh
+++ bash/filePermission.sh
@@ -3,9 +3,6 @@ file=text.txt
 # To check existence of the file
 [ -e $file ] && E="The file exists" || E="The file does NOT exists"
 echo "$E"
-
-# checking whether the file is writable or not and changing permissions
-
 [ -w $file ] && F="The file is already Writable" || F="The file is now Writable"
 chmod u+w $file 
 echo "$F"
