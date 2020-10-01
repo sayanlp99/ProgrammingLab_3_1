fedde94e4755ecc02f01c9d2abd57befa0304bf9
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2020-10-01 13:09:10 +0530

    Create fileGrep.sh

---
+++ bash/fileGrep.sh
@@ -0,0 +1,11 @@
+echo -n "Enter file name: "
+read f
+echo -n "Enter word to search: "
+read w
+
+echo ""
+echo "THE WORD '$w' FOUND IN:-"
+echo "(Line No. followd by lines)"
+echo "---------------------------"
+
+grep -n $w $f
